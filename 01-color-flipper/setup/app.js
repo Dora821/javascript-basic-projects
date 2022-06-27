4fd2be4de3901e3fbee308fe4984585f05e0ca07
Dora Xia <duruoxia1992@gmail.com>
2022-06-26 23:15:29 -0700

    finished project 1

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const color = document.querySelector(".color")
+const button = document.getElementById("btn")
+console.log('color', color)
+
+button.addEventListener('click', function() {
+  curColor = randomColor(colors);
+  color.textContent = curColor;
+  document.body.style.backgroundColor = curColor;
+})
+
+function randomColor(array){
+ return array[Math.floor(Math.random() * array.length)]
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,16 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const color = document.querySelector(".color")
+const button = document.getElementById("btn")
+
+button.addEventListener('click', function() {
+  let hexColor = '#'
+  for (let i=0; i < 6; i++) {
+    hexColor += getRandom(hex)
+  }
+  color.textContent = hexColor
+  document.body.style.backgroundColor = hexColor
+})
+
+function getRandom(colors) {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
